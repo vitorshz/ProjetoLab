5562a83af51256ba4afcb3fa0d217acf967d4596
vitorshz <110640686+vitorshz@users.noreply.github.com>
2024-08-06 20:07:04 -0300

    main

---
+++ ProjetoLab/src/main/java/br/unipar/projetolab/ProjetoLab.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unipar.projetolab;
+
+/**
+ *
+ * @author lucia
+ */
+public class ProjetoLab {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
