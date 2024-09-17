1e00eb24fc8675d74e974872450f2844f470fb46
vitorshz <110640686+vitorshz@users.noreply.github.com>
2024-09-17 20:29:38 -0300

    Revert "Update PacienteDAOImp.java"
    This reverts commit 017c307ab07747ac2501d687e57976006e785aeb.

--- ProjetoLab/src/main/java/br/unipar/projetolab/dao/PacienteDAOImp.java
+++ ProjetoLab/src/main/java/br/unipar/projetolab/dao/PacienteDAOImp.java
@@ -1,73 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package br.unipar.projetolab.dao;
 
-import br.unipar.projetolab.models.Paciente;
-import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
-
-
-public class PacienteDAOImp implements PacienteDAO{
-    
-    private EntityManager entityManager;
-
-    public PacienteDAOImp(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    @Override
-    public Paciente save(Paciente cliente) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-        entityManager.persist(cliente);
-        transaction.commit();
-        entityManager.close();
-        
-        System.out.println("Cliente salvo com sucesso!");
-        return cliente;
-    }
-
-    @Override
-    public Paciente update(Paciente cliente) {
-        EntityTransaction transaction = entityManager.getTransaction();
-        transaction.begin();
-        entityManager.merge(cliente);
-        transaction.commit();
-        entityManager.close();
-        
-        System.out.println("Cliente atualizado com sucesso!");
-        return cliente;
-    }
-
-    @Override
-    public Boolean delete(Paciente cliente) {        
-        EntityTransaction transaction = entityManager.getTransaction();
-        
-        try {
-            transaction.begin();
-            entityManager.remove(cliente);
-            transaction.commit();
-            entityManager.close();
-
-            System.out.println("Cliente removido com sucesso!");
-            return true;
-        } catch (Exception e) {
-            transaction.rollback();
-            System.out.println("Cliente atualizado com sucesso!");
-            return false;
-        }
-       
-    }
-
-    @Override
-    public Paciente findById(Integer id) {
-        return entityManager.find(Paciente.class, id);
-    }
-
-    @Override
-    public List<Paciente> findAll() {
-        return entityManager.createQuery("SELECT c FROM Cliente c",
-                Paciente.class).getResultList();
-                
-    }
+/**
+ *
+ * @author lucia
+ */
+public class PacienteDAOImp {
     
 }
