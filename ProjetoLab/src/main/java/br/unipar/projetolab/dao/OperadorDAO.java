129c13ca30d6f8ea2117523ea1fc329ba2d27387
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-09-27 17:24:04 -0300

    amen

---
+++ ProjetoLab/src/main/java/br/unipar/projetolab/dao/OperadorDAO.java
@@ -0,0 +1,14 @@
+package br.unipar.projetolab.dao;
+
+
+import br.unipar.projetolab.models.Operador;
+import java.util.List;
+
+public interface OperadorDAO {
+    Operador save(Operador operador);
+    Operador update(Operador operador);
+    Boolean delete(Operador operador);
+    
+    Operador findById(Long id);
+    List<Operador> findAll();
+}
