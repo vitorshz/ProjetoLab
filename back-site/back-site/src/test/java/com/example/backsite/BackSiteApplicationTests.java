4dc6681c94b0a3b0e09ad389a0364e1bc9a1c89f
vitorshz <110640686+vitorshz@users.noreply.github.com>
2024-11-21 22:00:01 -0300

    c

---
+++ back-site/back-site/src/test/java/com/example/backsite/BackSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
