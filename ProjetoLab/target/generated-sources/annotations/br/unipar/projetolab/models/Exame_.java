5f92440de064e8023f48aeafbe8af790bf09dc36
vitorshz <110640686+vitorshz@users.noreply.github.com>
2024-12-06 18:09:56 -0300

    c

--- ProjetoLab/target/generated-sources/annotations/br/unipar/projetolab/models/Exame_.java
+++ ProjetoLab/target/generated-sources/annotations/br/unipar/projetolab/models/Exame_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-22T17:56:02", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-11-29T19:18:48", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Exame.class)
 public class Exame_ { 
 
